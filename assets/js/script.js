f7c49a3acb822417d936fac11f33f9a73e49e306
natalia-lavarda <natalia.lavarda@gmail.com>
2021-12-15 22:06:11 -0300

    incluindo a fake api e contruindo os cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,3 +25,53 @@
     });
   };
 })();
+
+async function getInfos() {
+  const myHeaders = new Headers();
+
+  const myInit = {
+    method: "GET",
+    headers: myHeaders,
+  };
+
+  try {
+    const result = await fetch(
+      "http://localhost:3000/mulheresInspiradoras/",
+      myInit
+    );
+    const response = await result.json();
+    console.log(response);
+    response.forEach((mulher) => {
+      const cardSmall = document.createElement("div");
+      const inspiringWoman = document.createElement("h2");
+      const circle = document.createElement("div");
+      const imageCard = document.createElement("img");
+      const nacionalidade = document.createElement("h3");
+      const profissao = document.createElement("h3");
+      const idade = document.createElement("h3");
+
+      const containerGrid = document.querySelector(".container-grid");
+
+      containerGrid.appendChild(cardSmall);
+      cardSmall.appendChild(inspiringWoman);
+      cardSmall.appendChild(circle);
+      circle.appendChild(imageCard);
+      cardSmall.appendChild(nacionalidade);
+      cardSmall.appendChild(profissao);
+      cardSmall.appendChild(idade);
+
+      inspiringWoman.innerText = mulher.nome;
+      nacionalidade.innerText = mulher.nacionalidade;
+      profissao.innerText = mulher.profissao;
+      idade.innerText = mulher.idade;
+      imageCard.setAttribute("src", mulher.foto);
+
+      cardSmall.classList.add("card-small");
+      inspiringWoman.classList.add("inspiring-woman");
+      circle.classList.add("circle");
+      imageCard.classList.add("image-card");
+    });
+  } catch (error) {
+    console.log("Something is not working :(");
+  }
+}
